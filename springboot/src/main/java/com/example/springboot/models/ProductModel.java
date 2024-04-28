e2944781bda1807e7885b0c731cfd1ca79385497
Matheus Rosmaninho <matheusrosmano@gmail.com>
2024-04-28 18:52:01 -0300

    feat: Cria hiperlinks nos endpoints

--- springboot/src/main/java/com/example/springboot/models/ProductModel.java
+++ springboot/src/main/java/com/example/springboot/models/ProductModel.java
@@ -1,15 +1,15 @@
 package com.example.springboot.models;
 
 import jakarta.persistence.*;
+import org.springframework.hateoas.RepresentationModel;
 
-import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.UUID;
 
 @Entity
 @Table(name = "product")
-public class ProductModel implements Serializable {
+public class ProductModel extends RepresentationModel<ProductModel> implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
