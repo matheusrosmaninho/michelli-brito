5915be93d72aebaa81a8507ff9007149ecc6103a
Matheus Rosmaninho <matheusrosmano@gmail.com>
2024-04-28 17:57:51 -0300

    feat: Cria delete de produto

--- springboot/src/main/java/com/example/springboot/controllers/ProductController.java
+++ springboot/src/main/java/com/example/springboot/controllers/ProductController.java
@@ -55,4 +55,13 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(product));
     }
 
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable(value = "id") UUID id ) {
+        Optional<ProductModel> productValue = productRepository.findById(id);
+        if (productValue.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        productRepository.delete(productValue.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully.");
+    }
 }
